8f1c6a51692ce76d59f58262d2d47a9a84a7ea6d
Anna Kudriavtsev <anna328p@gmail.com>
2019-12-28 13:24:13 -0800

    feat(util): add echo command

--- bot.rb
+++ bot.rb
@@ -1,3 +1,24 @@
 #!/usr/bin/env ruby
 
 Bundler.require :default
+
+token = ENV['TOKEN']
+raise 'No token in environment; set TOKEN' unless token
+
+bot = Discordrb::Commands::CommandBot.new(
+  token: token,
+  prefix: '.',
+  command_doesnt_exist_message: 'Invalid command.'
+)
+
+bot.command :echo, {
+  help_available: true,
+  description: 'Echoes a string',
+  usage: '.echo <string>',
+  min_args: 1
+} do |_e, *args|
+  args.join(' ')
+end
+
+bot.run true
+bot.join
