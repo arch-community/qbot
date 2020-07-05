0beced4e20272830292b6ff1faadd7429d7bba96
Anna Kudriavtsev <anna328p@gmail.com>
2020-07-04 17:30:11 -0700

    fix(queries): check server id of query, use embeds properly

--- db/migrate/create_queries.rb
+++ db/migrate/create_queries.rb
@@ -1,6 +1,7 @@
 class CreateQueries < ActiveRecord::Migration[5.0]
   def change
     create_table :queries do |t|
+      t.column :server, :integer
       t.column :author, :integer
       t.column :text, :string
       t.timestamps
