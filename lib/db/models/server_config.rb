072134df7ae9d39586b379d3cb7448679b50c769
Anna Kudriavtsev <anna328p@gmail.com>
2021-01-21 18:45:52 -0800

    refactor(db): split large db file into separate files

---
+++ lib/db/models/server_config.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+# Helpers for the server config
+class ServerConfig < ActiveRecord::Base
+  # Cache config objects
+  def self.[](server_id)
+    # rubocop: disable Style/ClassVars
+    @@configs ||= {}
+    @@configs[server_id] ||= ServerConfig.find_or_create_by(server_id: server_id)
+  end
+
+  after_save do |conf|
+    @@configs.delete(conf.server_id)
+    # rubocop: enable Style/ClassVars
+  end
+
+  def modules_conf
+    modules_json ? JSON.parse(modules_json) : { 'disabled' => [] }
+  end
+
+  def server_prefix
+    unless prefix
+      prefix = QBot.config.global.prefix || '.'
+      save!
+    end
+
+    prefix
+  end
+
+  def modules
+    global = QBot.config.global.modules
+    global - modules_conf['disabled']
+  end
+end
