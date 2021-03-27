62191aa4db14a525104e9ca6de6c9c085350df0c
Fjorge <30599715+FjorgeTheHuman@users.noreply.github.com>
2021-03-27 18:12:37 +0000

    i18n: add en_kawaii to list of possible locales

--- lib/i18n.rb
+++ lib/i18n.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 I18n.load_path << Dir["#{File.expand_path('lib/locales')}/*.yml"]
-I18n.config.available_locales = %i[en tp]
+I18n.config.available_locales = %i[en tp en_kawaii]
 I18n.default_locale = :en
 
 def t(tid, *fields)
