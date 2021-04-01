adb001d681fb93a92ed938cdfc1c0345ae9f288c
Anna Kudriavtsev <anna328p@gmail.com>
2021-04-01 15:01:21 -0700

    feat: fun module, respond to pings

---
+++ modules/fun.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+##
+# Fun commmands
+module Fun
+  extend Discordrb::Commands::CommandContainer
+end
+
+QBot.bot.mention do |event|
+  event.respond(QBot.config.ping_emote || "I got pinged :o")
+end
