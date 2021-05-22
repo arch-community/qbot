5e95c37c0bbc58a514ce8e5f5cb597e0add488e1
Anna Kudriavtsev <anna328p@gmail.com>
2021-05-22 15:11:14 -0700

    feat: sitelen pona user glyphs

--- modules/sitelenpona.rb
+++ modules/sitelenpona.rb
@@ -24,13 +24,55 @@ module Sitelenpona
     end
   end
 
+  def self.replacement_glyphs(user)
+    [UserConfig[user.id].contents['sitelenpona']['glyphs'].split, nil]
+  rescue StandardError
+    [false, user.username.capitalize]
+  end
+
+  def self.glyph_style(author)
+    fn = UserConfig[author.id].contents['sitelenpona']['fontface']
+    font = SPGen.font_metadata.find { _1[:typeface] == fn }
+    font[:glyph_style]
+  rescue StandardError
+    :brackets
+  end
+
+  def self.name_text(user, style)
+    glyphs, fallback = replacement_glyphs(user)
+
+    return "[#{fallback}]" unless glyphs
+
+    case style
+    when :cartouche then "[_#{glyphs.join '_'}]"
+    when :brackets then "[#{glyphs.join ' '}]"
+    when :none then glyphs.join ' '
+    else glyphs.join ' '
+    end
+  end
+
+  def self.replace_all(text, users, style)
+    users.each do |user|
+      m = user.mention
+      text.gsub! m, name_text(user, style)
+      text.gsub! m.insert(2, '!'), name_text(user, style)
+    end
+
+    text
+  end
+
   command :sp, {
     help_available: true,
     usage: '.sp <text>',
     min_args: 1
   } do |event, _|
     msg = event.message
-    text = strip_command(msg.text, event.command.name)
+    text = replace_all(
+      strip_command(msg.text, event.command.name),
+      msg.mentions,
+      glyph_style(msg.author)
+    )
+    p text
 
     sanitized = Rails::Html::FullSanitizer.new.sanitize(text)
 
