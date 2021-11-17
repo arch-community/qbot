619692aa33acfe00372f6d2911bcb1989a7982ca
Anna <anna328p@gmail.com>
2021-11-16 22:58:17 -0600

    fix(bottom): rename debottom to regress

--- modules/bottom.rb
+++ modules/bottom.rb
@@ -1,24 +1,26 @@
 # frozen_string_literal: true
 
+##
+# uwu?
 module Bottom
   extend Discordrb::Commands::CommandContainer
-  
+
   command :bottom, {
-    aliases: [:b],
+    aliases: %i[b],
     help_available: true,
     usage: '.b <text>',
     min_args: 1
-  } do |event, *args|
+  } do |_, *args|
     text = args.join(' ').gsub('@', "\\@\u200D")
     embed Bottom.encode text
   end
 
-  command :debottom, {
-    aliases: [:db, :ub],
+  command :regress, {
+    aliases: %i[rg debottom db ub],
     help_available: true,
     usage: '.db <text>',
     min_args: 1
-  } do |event, *args|
+  } do |_, *args|
     input = args.join(' ')
     text = Bottom.decode(input).gsub('@', "\\@\u200D")
     embed text
