7b5616a203ba756efde88664ef5d9a214371bade
Anna <anna328p@gmail.com>
2021-11-27 02:56:17 -0600

    chore: add nix flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "qbot flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in rec {
+        packages.qbot = pkgs.callPackage ./. {};
+        defaultPackage = packages.qbot;
+
+        apps.qbot = flake-utils.lib.mkApp { drv = packages.qbot; };
+        defaultApp = apps.qbot;
+
+        legacyPackages.qbot = packages.qbot;
+        overlays = self: super: { qbot = packages.qbot; };
+      }
+    );
+}
