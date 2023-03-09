078e20d5562f8d546d25984bf37104d2b91ab5fd
Anna Kudriavtsev <anna328p@gmail.com>
2023-03-08 18:00:05 -0600

    chore: rework flake.nix

--- flake.nix
+++ flake.nix
@@ -1,47 +1,43 @@
 {
-  description = "qbot flake";
+	description = "qbot flake";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
+		flake-utils.url = "github:numtide/flake-utils";
+	};
 
-  outputs = { self, nixpkgs, flake-utils }:
-    rec {
-      overlay = final: prev: let
-        pkgs = import nixpkgs { 
-        	inherit (prev) system;
-        	config.allowUnfree = true;
-        };
-      in rec {
-        qbot = pkgs.callPackage ./. { };
-      };
+	outputs = { self
+			, nixpkgs
+			, flake-utils }:
+		rec {
+			overlay = final: prev: {
+				qbot = final.callPackage ./. { };
+			};
 
-      nixosModule = import ./module.nix;
-      nixosModules = [ nixosModule ];
-    } //
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          overlays = [ self.overlay ];
-          inherit system;
-          config.allowUnfree = true;
-        };
+			nixosModules = rec {
+				qbot = import ./module.nix;
+				default = qbot;
+			};
+		} //
+	flake-utils.lib.eachDefaultSystem (system:
+		let
+			pkgs = import nixpkgs {
+				inherit system;
+				config.allowUnfree = true;
+			};
 
-        pkg = pkgs.qbot;
-        app = flake-utils.lib.mkApp { drv = pkg; name = "qbot"; };
-        shell = import ./shell.nix { inherit pkgs; };
-      in rec {
-        packages.qbot = pkg;
-        packages.default = pkg;
+			pkg = pkgs.callPackage ./. { };
+			app = flake-utils.lib.mkApp { drv = pkg; name = "qbot"; };
+			shell = import ./shell.nix { inherit pkgs; };
+		in rec {
+			packages.qbot = pkg;
+			packages.default = pkg;
 
-        legacyPackages.qbot = pkg;
+			apps.qbot = app;
+			apps.default = app;
 
-        apps.qbot = app;
-        apps.default = app;
-
-        devShells.qbot = shell;
-        devShells.default = shell;
-      }
-    );
+			devShells.qbot = shell;
+			devShells.default = shell;
+		}
+	);
 }
