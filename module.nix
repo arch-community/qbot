68690da67bb51e8852c5fc553e5d1efe021a0000
Anna Kudriavtsev <anna328p@gmail.com>
2023-03-13 19:37:50 -0500

    chore: add option checking to nixos module

--- module.nix
+++ module.nix
@@ -3,23 +3,47 @@
 { lib, pkgs, config, ... }:
 
 with lib; let
-  format = pkgs.formats.json { };
-
   cfg = config.services.qbot;
 
   configFile = pkgs.writeText "config.json" (builtins.toJSON cfg.config);
 
 in {
-  options.services.qbot = {
+  options.services.qbot = with types; {
     enable = mkEnableOption "qbot service";
 
     package = mkPackageOption pkgs "qbot" { };
 
-    config = mkOption {
-      default = {};
-      description = "Configuration for qbot";
-      type = format.type;
-    };
+	config = let
+		mkOpt' = type: description: default: rest:
+			mkOption ({ inherit type description default; } // rest);
+
+		mkOpt = t: d: mkOpt' t d null;
+
+	in {
+		token = mkOpt str "Discord gateway bot token" { };
+		client_id = mkOpt int "Discord application client ID" { };
+		owner = mkOpt int "Discord user ID of the bot's owner" { };
+
+		database = {
+			type = mkOpt' (enum [ "sqlite3" "oracle_enhanced" ])
+				"Type of database to connect to" "sqlite3" { };
+
+			db = mkOpt' str "Database name or path" "db.sqlite3" { };
+
+			user = mkOpt (nullOr str) "Database username" { };
+			pass = mkOpt (nullOr str) "Database password" { };
+		};
+
+		my_repo = mkOpt' str "The bot's VCS repository"
+			"https://github.com/arch-community/qbot" { };
+
+		modules = mkOpt (listOf str) "List of modules to load" { };
+
+		default_prefix = mkOpt' str "Default command prefix" "." { };
+
+		bot_id_allowlist = mkOpt (listOf int)
+			"User IDs that ignore the bot filter" { };
+	};
   };
 
   config = mkIf cfg.enable {
