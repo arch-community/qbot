f0828822f011d0a7621f5b1280cdf1ca75d2e204
Anna Kudriavtsev <anna328p@gmail.com>
2023-09-24 14:39:53 -0500

    chore: clean up module.nix

--- module.nix
+++ module.nix
@@ -2,67 +2,81 @@
 
 { lib, pkgs, config, ... }:
 
-with lib; let
-  cfg = config.services.qbot;
+let
+    cfg = config.services.qbot;
 
-  configFile = pkgs.writeText "config.json" (builtins.toJSON cfg.config);
+    configFile = pkgs.writeText "config.json" (builtins.toJSON cfg.config);
 
 in {
-  options.services.qbot = with types; {
-    enable = mkEnableOption "qbot service";
+    options.services.qbot = let
+        inherit (lib)
+            mkOption mkEnableOption mkPackageOption;
+    in {
+        enable = mkEnableOption "qbot service";
 
-    package = mkPackageOption pkgs "qbot" { };
+        package = mkPackageOption pkgs "qbot" { };
 
-	config = let
-		mkOpt' = type: description: default: rest:
-			mkOption ({ inherit type description default; } // rest);
+        config = let
+            inherit (lib.types)
+                str int enum nullOr listOf;
 
-		mkOpt = t: d: mkOpt' t d null;
+            mkOpt' = type: description: default: rest:
+                mkOption ({ inherit type description default; } // rest);
 
-	in {
-		token = mkOpt str "Discord gateway bot token" { };
-		client_id = mkOpt int "Discord application client ID" { };
-		owner = mkOpt int "Discord user ID of the bot's owner" { };
+            mkOpt = t: d: mkOpt' t d null;
 
-		database = {
-			type = mkOpt' (enum [ "sqlite3" "oracle_enhanced" ])
-				"Type of database to connect to" "sqlite3" { };
+        in {
+            token = mkOpt str "Discord gateway bot token" { };
+            client_id = mkOpt int "Discord application client ID" { };
+            owner = mkOpt int "Discord user ID of the bot's owner" { };
 
-			db = mkOpt' str "Database name or path" "db.sqlite3" { };
+            database = {
+                type = mkOpt' (enum [ "sqlite3" "oracle_enhanced" ])
+                    "Type of database to connect to" "sqlite3" { };
 
-			user = mkOpt (nullOr str) "Database username" { };
-			pass = mkOpt (nullOr str) "Database password" { };
-		};
+                db = mkOpt' str "Database name or path" "db.sqlite3" { };
 
-		my_repo = mkOpt' str "The bot's VCS repository"
-			"https://github.com/arch-community/qbot" { };
+                user = mkOpt (nullOr str) "Database username" { };
+                pass = mkOpt (nullOr str) "Database password" { };
+            };
 
-		modules = mkOpt (listOf str) "List of modules to load" { };
+            my_repo = mkOpt' str "The bot's VCS repository"
+                "https://github.com/arch-community/qbot" { };
 
-		default_prefix = mkOpt' str "Default command prefix" "." { };
+            modules = mkOpt (listOf str) "List of modules to load" { };
 
-		bot_id_allowlist = mkOpt (listOf int)
-			"User IDs that ignore the bot filter" { };
-	};
-  };
+            default_prefix = mkOpt' str "Default command prefix" "." { };
 
-  config = mkIf cfg.enable {
-    systemd.services.qbot = {
-      description = "qbot discord bot";
-
-      wantedBy = [ "multi-user.target" ];
-      after = [ "network.target" ];
-
-      serviceConfig = {
-        ExecStart = "${getExe cfg.package} -c ${configFile} --state-dir \${STATE_DIRECTORY} --no-console";
-        StateDirectory = "qbot";
-        DynamicUser = true;
-      };
+            bot_id_allowlist = mkOpt (listOf int)
+                "User IDs that ignore the bot filter" { };
+        };
     };
 
-	nix.settings = {
-		substituters = [ nixConfig.extra-substituters ];
-		trusted-public-keys = [ nixConfig.extra-trusted-public-keys ];
-	};
-  };
+    config = let
+        inherit (lib) mkIf getExe;
+    in mkIf cfg.enable {
+        systemd.services.qbot = {
+            description = "qbot discord bot";
+
+            wantedBy = [ "multi-user.target" ];
+            after = [ "network.target" ];
+
+            serviceConfig = {
+                ExecStart = "${getExe cfg.package}"
+                    + " -c ${configFile}"
+                    + " --state-dir \${STATE_DIRECTORY}"
+                    + " --no-console";
+
+                StateDirectory = "qbot";
+                DynamicUser = true;
+            };
+        };
+
+        nix.settings = {
+            substituters = [ nixConfig.extra-substituters ];
+            trusted-public-keys = [ nixConfig.extra-trusted-public-keys ];
+        };
+    };
 }
+
+# vim: et ts=4 sw=4
