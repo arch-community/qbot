460aae45cefb388a50d255d1775463c10fd04ddd
Hm7 <212261199+User7-hm@users.noreply.github.com>
2025-08-10 22:17:14 +0300

    Format nix files with the official formatter `nixfmt`

--- default.nix
+++ default.nix
@@ -1,69 +1,98 @@
-{ stdenv, lib, makeWrapper
-, fetchFromGitHub, gitignoreSource
-, ruby, bundler, bundix, bundlerEnv, defaultGemConfig
-, rustPlatform, cargo, rustc, fetchgit
-, libsodium, libopus, imagemagick }:
+{
+  stdenv,
+  lib,
+  makeWrapper,
+  fetchFromGitHub,
+  gitignoreSource,
+  ruby,
+  bundler,
+  bundix,
+  bundlerEnv,
+  defaultGemConfig,
+  rustPlatform,
+  cargo,
+  rustc,
+  fetchgit,
+  libsodium,
+  libopus,
+  imagemagick,
+}:
 
 let
-	env = bundlerEnv {
-		name = "qbot-bundler-env";
-		gemdir = ./.;
-		inherit ruby;
+  env = bundlerEnv {
+    name = "qbot-bundler-env";
+    gemdir = ./.;
+    inherit ruby;
 
-		gemConfig = defaultGemConfig // {
-			tantiny = attrs: {
-				cargoDeps = rustPlatform.fetchCargoTarball {
-					src = fetchgit {
-						inherit (attrs.source)
-							url rev sha256 fetchSubmodules;
-					};
+    gemConfig = defaultGemConfig // {
+      tantiny = attrs: {
+        cargoDeps = rustPlatform.fetchCargoTarball {
+          src = fetchgit {
+            inherit (attrs.source)
+              url
+              rev
+              sha256
+              fetchSubmodules
+              ;
+          };
 
-					sha256 = "JlPkdrU2fq+0v/2QJnqtSEv3bqiJbdAvzK3NrrMdY8A=";
-				};
+          sha256 = "JlPkdrU2fq+0v/2QJnqtSEv3bqiJbdAvzK3NrrMdY8A=";
+        };
 
-				nativeBuildInputs = [
-					rustPlatform.cargoSetupHook cargo rustc
-				];
+        nativeBuildInputs = [
+          rustPlatform.cargoSetupHook
+          cargo
+          rustc
+        ];
 
-				postUnpack = ''
-					mv .cargo tantiny*
-				'';
-			};
-		};
-	};
+        postUnpack = ''
+          					mv .cargo tantiny*
+          				'';
+      };
+    };
+  };
 
-in stdenv.mkDerivation rec {
-	name = "qbot";
+in
+stdenv.mkDerivation rec {
+  name = "qbot";
 
-	src = gitignoreSource ./.;
+  src = gitignoreSource ./.;
 
-	nativeBuildInputs = [ makeWrapper ];
-	buildInputs = [ env.wrappedRuby imagemagick ];
-	propagatedBuildInputs = [ libopus libsodium ];
+  nativeBuildInputs = [ makeWrapper ];
+  buildInputs = [
+    env.wrappedRuby
+    imagemagick
+  ];
+  propagatedBuildInputs = [
+    libopus
+    libsodium
+  ];
 
-	passthru = {
-		fontconfigFile = "${src}/share/fc-config.xml";
-		binPath = lib.makeBinPath buildInputs;
-		libPath = lib.makeLibraryPath propagatedBuildInputs;
-	};
+  passthru = {
+    fontconfigFile = "${src}/share/fc-config.xml";
+    binPath = lib.makeBinPath buildInputs;
+    libPath = lib.makeLibraryPath propagatedBuildInputs;
+  };
 
-	installPhase = let
-		inherit (passthru) binPath libPath fontconfigFile;
-	in ''
-		mkdir -p $out/{bin,share}
-		cp -r . $out/share/qbot
+  installPhase =
+    let
+      inherit (passthru) binPath libPath fontconfigFile;
+    in
+    ''
+      		mkdir -p $out/{bin,share}
+      		cp -r . $out/share/qbot
 
-		makeWrapper $out/share/qbot/qbot $out/bin/qbot \
-			--set FONTCONFIG_FILE '${fontconfigFile}' \
-			--prefix PATH : '${binPath}' \
-			--prefix LD_LIBRARY_PATH : '${libPath}'
-	'';
+      		makeWrapper $out/share/qbot/qbot $out/bin/qbot \
+      			--set FONTCONFIG_FILE '${fontconfigFile}' \
+      			--prefix PATH : '${binPath}' \
+      			--prefix LD_LIBRARY_PATH : '${libPath}'
+      	'';
 
-	meta = with lib; {
-		description = "General purpose Discord bot";
-		homepage = "https://github.com/arch-community/qbot";
-		license = licenses.agpl3Plus;
-		maintainers = with maintainers; [ anna328p ];
-		mainProgram = "qbot";
-	};
+  meta = with lib; {
+    description = "General purpose Discord bot";
+    homepage = "https://github.com/arch-community/qbot";
+    license = licenses.agpl3Plus;
+    maintainers = with maintainers; [ anna328p ];
+    mainProgram = "qbot";
+  };
 }
--- shell.nix
+++ shell.nix
@@ -1,16 +1,18 @@
 { pkgs, pkg }:
 
 pkg.overrideAttrs (oa: {
-	nativeBuildInputs = oa.nativeBuildInputs ++ (with pkgs; [
-		git
-		graphviz
-		loc
-		(sqlite.override { interactive = true; })
-		yq-go
-	]);
+  nativeBuildInputs =
+    oa.nativeBuildInputs
+    ++ (with pkgs; [
+      git
+      graphviz
+      loc
+      (sqlite.override { interactive = true; })
+      yq-go
+    ]);
 
-	BUNDLE_FORCE_RUBY_PLATFORM = "1";
+  BUNDLE_FORCE_RUBY_PLATFORM = "1";
 
-	LD_LIBRARY_PATH = oa.passthru.libPath;
-	FONTCONFIG_FILE = oa.passthru.fontconfigFile;
+  LD_LIBRARY_PATH = oa.passthru.libPath;
+  FONTCONFIG_FILE = oa.passthru.fontconfigFile;
 })
